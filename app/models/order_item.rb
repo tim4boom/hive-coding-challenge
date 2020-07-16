565c7119a18f239df48746ae15527c34f444a7ae
Tim Vierboom <tim-vierboom@gmx.de>
2020-07-16 11:03:33 +0200

    created OrderItems model and added validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   #TODO
-
-
+  validates :product_name, :order_id, presence: true
+  validates_length_of :product_name, minimum: 1, maximum: 55
 end
